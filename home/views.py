5102c560dbe1cb3894215b434cd1453fb44d9220
gomathishankar28 <gomathishankar28@gmail.com>
2021-10-26 12:11:20 +0000

    added new products to latest arrival section

--- home/views.py
+++ home/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    latest_arrival_1 = get_object_or_404(Product, pk=6)
+    latest_arrival_2 = get_object_or_404(Product, pk=64)
+    latest_arrival_3 = get_object_or_404(Product, pk=65)
+
+    template = 'home/index.html'
+    context = {
+        'latest_arrival_1': latest_arrival_1,
+        'latest_arrival_2': latest_arrival_2,
+        'latest_arrival_3': latest_arrival_3,
+    }
+
+    return render(request, template, context)
