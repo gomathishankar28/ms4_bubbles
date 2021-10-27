be105d49458a4846fc52cfa013b60dc14e29bc78
gomathishankar28 <gomathishankar28@gmail.com>
2021-10-27 13:28:00 +0000

    changed background image for home page on small screen

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ def index(request):
     latest_arrival_2 = get_object_or_404(Product, pk=64)
     latest_arrival_3 = get_object_or_404(Product, pk=65)
 
-    template = 'home/index.html'
+    template = 'home/index1.html'
     context = {
         'latest_arrival_1': latest_arrival_1,
         'latest_arrival_2': latest_arrival_2,
