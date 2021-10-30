eec26e6404ee30f5dfe511f3f4fad3a50df0bb6b
gomathishankar28 <gomathishankar28@gmail.com>
2021-10-30 12:31:33 +0000

    creted models and forms for reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Review(models.Model):
+    """
+    Creates a review model to allow user to perform
+    CRUD operations on product reviews
+    """
+
+    RATE = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    rating = models.IntegerField(choices=RATE)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    review_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
