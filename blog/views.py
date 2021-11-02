e8641910bf378057f5d4474f4f690ffda0549234
gomathishankar28 <gomathishankar28@gmail.com>
2021-11-02 13:06:24 +0000

    added blog.html and post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Blog, Post, Comment
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import CommentForm
+from .forms import PostForm
+
+
+def blog(request):
+    """ BLog page to display all available posts """
+
+    posts = Post.objects.all()
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts
+    }
+    
+    return render(request, template, context)
+
+def post_detail(request, slug):
+    """ Display each Post in detail along with its comments """
+    post = Post.objects.get(slug=slug)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.post = post
+            obj.save()
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = CommentForm()
+
+    template = 'blog/blog_detail.html'
+    context = {
+        'post': post,
+        'form': form,
+    }
+
+    return render(request, template, context)
