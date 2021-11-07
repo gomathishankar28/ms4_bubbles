d91e39ebed5088a45dc12dbbe6e7b2b16b5cab11
gomathishankar28 <gomathishankar28@gmail.com>
2021-11-07 18:55:49 +0000

    created manage profile page to display personal and shipping details

--- profiles/views.py
+++ profiles/views.py
@@ -12,14 +12,14 @@ def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
-    template = 'profiles/profile1.html'
-    orders = profile.orders.filter.latest()
+    template = 'profiles/profile.html'
+    order = profile.orders.latest('order_number')
     context = {
         'profile': profile,
         'on_profile_page': True,
-        'orders': orders,
+        'order': order,
     }
-
+    print(order)
     return render(request, template, context)
 
 
