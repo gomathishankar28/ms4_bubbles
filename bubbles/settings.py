302d436deee6a062deb530c1a4af092d2e15011b
gomathishankar28 <gomathishankar28@gmail.com>
2021-11-11 13:27:31 +0000

    changed method name on signals.py

--- bubbles/settings.py
+++ bubbles/settings.py
@@ -124,13 +124,14 @@ WSGI_APPLICATION = 'bubbles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
-        'default': {
+       'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
