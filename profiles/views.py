cf4fd8bdaf49a4990a30637dfc60fcb984441e70
gomathishankar28 <gomathishankar28@gmail.com>
2021-12-01 12:56:18 +0000

    used CDN for animate.css and removed the node_modules for it

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
